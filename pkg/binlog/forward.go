b4f02c028a912bb2415661482f6525443211d12a
siddontang <siddontang@gmail.com>
2015-05-05 16:22:39 +0800

    add forward handler and NewSnapshotFunc

--- pkg/binlog/forward.go
+++ pkg/binlog/forward.go
@@ -3,8 +3,50 @@
 
 package binlog
 
+import (
+	"github.com/reborndb/go/log"
+)
+
 type Forward struct {
 	DB   uint32
 	Op   string
 	Args []interface{}
+}
+
+type ForwardHandler func(f *Forward) error
+
+// Register the handler that will be called before db storage commit
+func (b *Binlog) RegBeforeCommitHandler(h ForwardHandler) {
+	if err := b.acquire(); err != nil {
+		return
+	}
+	defer b.release()
+
+	b.beforeCommitHandlers = append(b.beforeCommitHandlers, h)
+}
+
+// Register the handler that will be called after db storage committed
+func (b *Binlog) RegAfterCommitHandler(h ForwardHandler) {
+	if err := b.acquire(); err != nil {
+		return
+	}
+	defer b.release()
+
+	b.afterCommitHandlers = append(b.afterCommitHandlers, h)
+}
+
+func (b *Binlog) travelBeforeCommitHandlers(f *Forward) {
+	for _, h := range b.beforeCommitHandlers {
+		if err := h(f); err != nil {
+			log.WarnErrorf(err, "handle WillCommitHandler err")
+		}
+	}
+}
+
+func (b *Binlog) travelAfterCommitHandlers(f *Forward) {
+	for _, h := range b.afterCommitHandlers {
+		if err := h(f); err != nil {
+			log.WarnErrorf(err, "handle DidCommitHandler err")
+		}
+	}
 }
