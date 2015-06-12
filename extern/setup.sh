9f7164dbca15101ab32537a5d9cada084fc7b416
qiuyesuifeng <cuiqiu.bupt@gmail.com>
2015-06-12 16:58:53 +0800

    Add bootstrap.sh for easily setup; Remove race flag temporarily

--- extern/setup.sh
+++ extern/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "setup start!"
+
 rm -rf tmp; mkdir -p tmp
 BUILD=`pwd`/tmp
 
@@ -8,12 +10,19 @@ make -j4 && cd .. || exit 1
 
 cp -r leveldb/include/leveldb ${BUILD}
 cp leveldb/libleveldb.a ${BUILD}
+cp leveldb/libleveldb.so.1.18 ${BUILD}
 
 cd ./rocksdb; make clean
-make -j4 shared_lib && cd .. || exit 1
+make -j4 static_lib shared_lib && cd .. || exit 1
 
 cp -rf rocksdb/include/rocksdb ${BUILD}
-cp -f rocksdb/librocksdb.dylib ${BUILD}
+cp rocksdb/librocksdb.a ${BUILD}
+cp rocksdb/librocksdb.so ${BUILD}
+
+cd ./tmp
+ln -s libleveldb.so.1.18 libleveldb.so.1
+ln -s libleveldb.so.1.18 libleveldb.so
+cd .. || exit 1
 
 cd ./levigo
 CGO_CFLAGS="-I${BUILD}" CGO_LDFLAGS="-L${BUILD} -lleveldb -lsnappy -lstdc++" go install ./
@@ -22,3 +31,5 @@ cd .. || exit 1
 cd ./gorocks
 CGO_CFLAGS="-I${BUILD}" CGO_LDFLAGS="-L${BUILD} -lrocksdb -lsnappy -llz4 -lbz2 -lz -lm -lstdc++" go install ./
 cd .. || exit 1
+
+echo "setup success!"
