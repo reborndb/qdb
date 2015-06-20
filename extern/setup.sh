b642ca34b78a5f57f537e60994b60b0079d00d6c
siddontang <siddontang@gmail.com>
2015-06-20 17:09:43 +0800

    change engine setup bash

--- extern/setup.sh
+++ extern/setup.sh
@@ -1,26 +1,57 @@
 #!/bin/bash
 
-rm -rf tmp; mkdir -p tmp
-BUILD=`pwd`/tmp
+# print every running process 
+set -x
 
-# we need leveldb and rocksdb shared lib
+BUILD_PATH=`pwd`
 
-cd ./leveldb; make clean
-make -j4 && cd .. || exit 1
+# We will install rocksdb and leveldb in standard path
+INSTALL_PATH=/usr/local
 
-cp -r leveldb/include/leveldb ${BUILD}
-cp -f leveldb/libleveldb.* ${BUILD}
+CXX=gcc
 
-cd ./rocksdb; make clean
-make -j4 shared_lib static_lib && cd .. || exit 1
+# Test whether Snappy library is installed
+# We must install Snappy
+# http://code.google.com/p/snappy/
+    $CXX -x c++ - -o /dev/null 2>/dev/null  <<EOF
+      #include <snappy.h>
+      int main() {}
+EOF
+    if [ "$?" -ne 0 ]; then
+       echo "You must install Snappy library"
+       exit 1
+    fi
 
-cp -rf rocksdb/include/rocksdb ${BUILD}
-cp -f rocksdb/librocksdb.* ${BUILD}
+# Test whether LevelDB library is installed
+    $CXX -x c++ - -o /dev/null -lleveldb 2>/dev/null  <<EOF
+      #include <leveldb/c.h>
+      int main() {
+        leveldb_major_version();  
+      }
+EOF
+    if [ "$?" -ne 0 ]; then
+        cd $BUILD_PATH/leveldb && make -j4 
+        cp -rf $BUILD_PATH/leveldb/include/leveldb $INSTALL_PATH/include 
+        cp -f $BUILD_PATH/leveldb/libleveldb.* $INSTALL_PATH/lib
+    fi
 
-cd ./levigo
-CGO_CFLAGS="-I${BUILD}" CGO_LDFLAGS="-L${BUILD} -lleveldb -lsnappy" go install ./
-cd .. || exit 1
+# Test whether LevelDB library is installed
+    $CXX -x c++ - -o /dev/null -lrocksdb 2>/dev/null  <<EOF
+      #include <rocksdb/c.h>
+      int main() {
+        rocksdb_options_create();
+      }
+EOF
+    if [ "$?" -ne 0 ]; then
+        cd $BUILD_PATH/rocksdb && make -j4 shared_lib static_lib
+        cp -rf $BUILD_PATH/rocksdb/include/rocksdb $INSTALL_PATH/include 
+        cp -f $BUILD_PATH/rocksdb/librocksdb.* $INSTALL_PATH/lib
+    fi
 
-cd ./gorocks
-CGO_CFLAGS="-I${BUILD}" CGO_LDFLAGS="-L${BUILD} -lrocksdb -lsnappy -llz4 -lbz2 -lz -lm" go install ./
-cd .. || exit 1
+cd $BUILD_PATH/levigo 
+CGO_LDFLAGS="-lsnappy" go install ./
+
+cd $BUILD_PATH/gorocks 
+CGO_LDFLAGS="-lsnappy" go install ./
+
+cd $BUILD_PATH
