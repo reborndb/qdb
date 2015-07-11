9e9a2f3e593cb13adbe959ad0ad82e4bf2750a96
qiuyesuifeng <cuiqiu.bupt@gmail.com>
2015-07-11 12:02:26 +0800

    Update Godeps.json

--- Godeps/_workspace/src/github.com/syndtr/goleveldb/leveldb/db_write.go
+++ Godeps/_workspace/src/github.com/syndtr/goleveldb/leveldb/db_write.go
@@ -249,7 +249,8 @@ func (db *DB) Put(key, value []byte, wo *opt.WriteOptions) error {
 	return db.Write(b, wo)
 }
 
-// Delete deletes the value for the given key.
+// Delete deletes the value for the given key. It returns ErrNotFound if
+// the DB does not contain the key.
 //
 // It is safe to modify the contents of the arguments after Delete returns.
 func (db *DB) Delete(key []byte, wo *opt.WriteOptions) error {
--- Godeps/_workspace/src/github.com/syndtr/goleveldb/leveldb/table/writer.go
+++ Godeps/_workspace/src/github.com/syndtr/goleveldb/leveldb/table/writer.go
@@ -12,7 +12,7 @@ import (
 	"fmt"
 	"io"
 
-	"github.com/google/go-snappy/snappy"
+	"github.com/syndtr/gosnappy/snappy"
 
 	"github.com/syndtr/goleveldb/leveldb/comparer"
 	"github.com/syndtr/goleveldb/leveldb/filter"
